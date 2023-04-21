8268b24d50a3aaa162fef666cd11da2e72817bd9
De4oult <timofej.dimitrov@yandex.ru>
2023-04-21 19:10:34 +0300

    Unready code

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# container
-Golang container
+# Container
+
+This is a container built in __Golang__.
+The system deployed in a default container is __Kali Linux__.
+
+
+## Instructions
+
+Store your __ROOTFS__ to `containers/` and run `container.go` with cmd-args:
+```bash
+go run container.go run [name/of/rootfs] [command]
+```
+
+---
+### Example (default project):
+The default repository contains __Kali ROOTFS__ in `containers/`(`containers/kali`).
+
+```bash
+go run container.go run kali /bin/bash 
+```
+This command will run /bin/bash on a standard __Kali ROOTFS__.
+
---
+++ container.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		parent()
+	case "child":
+		child()
+	default:
+		panic("Unknown argument")
+	}
+}
+
+func parent() {
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+	}
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("Running container <%v> as PID %d\n", os.Args[3:], os.Getpid())
+
+	cmd := exec.Command(os.Args[3], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(syscall.Chroot("containers/" + os.Args[2]))
+	must(os.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+	must(cmd.Run())
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
